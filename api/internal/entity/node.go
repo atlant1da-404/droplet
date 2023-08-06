0168a34690f11f9f87e8a2ded99f520ac8d534d0
atlant1da-404 <kalnybolotchyk.01@gmail.com>
2023-08-06 06:53:08 +0300

    feat(api): add entity Node

---
+++ api/internal/entity/node.go
@@ -0,0 +1,12 @@
+package entity
+
+type Node struct {
+	Id                 string `json:"id" gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
+	SenderEmail        string `json:"senderEmail"`
+	SenderMacAddress   string `json:"senderMacAddress"`
+	ReceiverEmail      string `json:"receiverEmail"`
+	ReceiverMacAddress string `json:"ReceiverMacAddress"`
+}
+
+// Sender(Up server) - Backend - Receiver
+// Receiver send request to get file - Sender
